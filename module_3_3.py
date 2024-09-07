5ded177b3e44c31a04872d8bb400bbd3cb7c24fe
ilgiz091 <ilgiz091@mail.ru>
2024-09-07 17:05:11 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,14 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
+print_params()
+
+values_list = [1, 'цифра', False]
+values_dict = {'a': 3, 'b': 'булева', 'c': True}
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [54.32, 'Строка' ]
+print_params(*values_list_2, 42)
